7e8d4d24c3f0170c735fdd6c14b6f7852f366bf5
Pao <pzsigua@gmail.com>
2022-07-14 03:14:03 -0700

    users can now successfully join different rooms and chat

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 const { username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true
@@ -10,6 +12,13 @@ const socket = io();
 // Join a room
 socket.emit('joinRoom', { username, room });
 
+// room and users
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+// server message
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
@@ -39,4 +48,16 @@ function outputMessage(message) {
         ${message.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
+}
+
+// add room to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
 }
